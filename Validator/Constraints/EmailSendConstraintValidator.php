d1bdb9b30d7c9cd06819f4fa55662220b9b243de
Alex Kovalevych <alexkovalevych@gmail.com>
2014-02-14 01:29:09 +0200

    fix email send validator

--- Validator/Constraints/EmailSendConstraintValidator.php
+++ Validator/Constraints/EmailSendConstraintValidator.php
@@ -9,25 +9,25 @@ class EmailSendConstraintValidator extends ConstraintValidator
 {
     public function validate($object, Constraint $constraint)
     {
-        if (!$object->getIsTest()) {
-            if ($object->getUserLists()->count() == 0) {
-                return $this->context->addViolationAt('userLists', $constraint->invalidUserLists);
-            } else {
-                return;
+        if ($object->getIsTest()) {
+            if (!$object->getTestEmails()) {
+                return $this->context->addViolationAt('testEmails', $constraint->invalidTestSend);
             }
+
+            $filteredEmails = filter_var_array($object->getTestEmails(), FILTER_VALIDATE_EMAIL);
+            foreach ($filteredEmails as $key => $value) {
+                if (!$value) {
+                    $this->context->addViolationAt('testEmails', $constraint->invalidTestEmails, array(
+                        '%email%' => $object->getTestEmails()[$key]
+                    ));
+                }
+            }
+
+            return;
         }
 
-        if (!$object->getTestEmails()) {
-            return $this->context->addViolationAt('testEmails', $constraint->invalidTestSend);
-        }
-
-        $filteredEmails = filter_var_array($object->getTestEmails(), FILTER_VALIDATE_EMAIL);
-        foreach ($filteredEmails as $key => $value) {
-            if (!$value) {
-                $this->context->addViolationAt('testEmails', $constraint->invalidTestEmails, array(
-                    '%email%' => $object->getTestEmails()[$key]
-                ));
-            }
+        if ($object->getUserLists()->count() == 0) {
+            return $this->context->addViolationAt('userLists', $constraint->invalidUserLists);
         }
     }
 }
